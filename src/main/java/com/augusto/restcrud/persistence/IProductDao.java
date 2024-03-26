c128e8b492a6860950f453e47e9a4828ab284939
Augusto Salgado <augustocesarsalgado05@gmail.com>
2024-03-26 23:54:17 +0100

    Persistences of Category and Product created

---
+++ src/main/java/com/augusto/restcrud/persistence/IProductDao.java
@@ -0,0 +1,18 @@
+package com.augusto.restcrud.persistence;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.augusto.restcrud.models.Product;
+
+public interface IProductDao {
+
+    List<Product> findAll();
+
+    Optional<Product> findById(Long id);
+
+    void save(Product product);
+
+    void deleteById(Long id);
+
+}

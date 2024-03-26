9abb819e67e9bc187f164ded77bc53e25e2a7b40
Augusto Salgado <augustocesarsalgado05@gmail.com>
2024-03-26 21:06:31 +0100

    Model Product created

---
+++ src/main/java/com/augusto/restcrud/models/Product.java
@@ -0,0 +1,35 @@
+package com.augusto.restcrud.models;
+
+import java.math.BigDecimal;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "products")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "price")
+    private BigDecimal price;
+    
+
+}

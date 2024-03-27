2395ba6f36af202e126e38e47920c649a1d91277
Augusto Salgado <augustocesarsalgado05@gmail.com>
2024-03-27 15:21:29 +0100

    Method findProductByPriceBetween created

--- src/main/java/com/augusto/restcrud/repository/ProductRepository.java
+++ src/main/java/com/augusto/restcrud/repository/ProductRepository.java
@@ -1,5 +1,8 @@
 package com.augusto.restcrud.repository;
 
+import java.math.BigDecimal;
+
+import java.util.List;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +11,5 @@ import com.augusto.restcrud.models.Product;
 @Repository
 public interface ProductRepository extends CrudRepository<Product, Long> {
 
+    List<Product> findProductByPriceBetween(BigDecimal minPrice, BigDecimal maxPrice);
 }
